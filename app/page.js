f03e76ddc00cc5f212587607d18c27e1bdf207aa
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2025-03-05 05:30:33 +0100

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,89 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="flex flex-col items-center justify-center bg-black min-h-screen p-10 space-y-10">
+      {/* Light Radial */}
+      <section className="flex flex-wrap gap-4 justify-center">
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2xl rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(to bottom left, #FDB00B 0%, #FDA70F 36%, #FB8121 80%, #D24856 100%)",
+          }}
+        >
+          48-hour <br />
+          iteration cycles
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2x rounded-3xl"
+          style={{
+            background:
+            "linear-gradient(to top right, #66259D 0%, #B51F69 13%, #EC1B44 25%, #FBAA08 73%, #FBAA08 100%)",
+        }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          TEST <br />
+          iteration cycles
+        </div>
+     
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2xl rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(to bottom left, #66259D 0%, #B51F69 13%, #EC1B44 25%, #FBAA08 73%, #FBAA08 100%)",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          TEST <br />
+          iteration cycles
+        </div>
+      </section>
+
+      {/* Dark Radial */}
+      <section className="flex flex-wrap gap-4 justify-center">
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2xl rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(to bottom left, #4E0061 0%, #20006F 44%, #003D6F 63%, #00634F 100%)",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          48-hour <br />
+          iteration cycles
+        </div>
+
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2xl rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(to bottom left, #0D4504 0%, #211B1B 39%, #271022 63%, #2B0606 100%)",
+          }}
+        >
+          48-hour <br />
+          iteration cycles
+        </div>
+
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2xl rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(to bottom left, #4A3400 0%, #322A16 39%, #092727 63%, #001D35 100%)",
+          }}
+        >
+          48-hour <br />
+          iteration cycles
+        </div>
+
+        <div
+          className="w-[360px] h-[470px] flex items-center justify-center text-white font-bold text-2xl rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(to bottom left, #00384E 0%, #270C2A 53%, #2A0001 100%)",
+          }}
+        >
+          48-hour <br />
+          iteration cycles
+        </div>
+      </section>
+    </section>
   );
 }
